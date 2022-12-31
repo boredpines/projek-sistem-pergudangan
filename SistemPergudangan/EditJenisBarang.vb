5b998cb235a109a5fd8fb355bcbfdb7b6111e256
fatihbadriawan <95902904+fatihbadriawan@users.noreply.github.com>
2022-12-31 15:02:22 +0700

    Revert "Jenis Barang"
    This reverts commit 4b6be0aab26e1bc2ea4e044aafcc25b05988ae95.

--- SistemPergudangan/EditJenisBarang.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class EditJenisBarang
-    Private Sub BtnTambah_Click(sender As Object, e As EventArgs) Handles BtnTambah.Click
-        JenisBarang.JenisBarang.GSIdJenis = Integer.Parse(TxtIdJenis.Text)
-        JenisBarang.JenisBarang.GSNamaJenis = TxtNamaJenis.Text.ToString()
-        JenisBarang.JenisBarang.GSSatuanJenis = CbSatuanJenis.SelectedItem().ToString()
-    End Sub
-End Class
